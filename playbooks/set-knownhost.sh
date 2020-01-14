248d9abcde77ea36ca1156e3358cb97092d77118
Michael <michael.named@gmail.com>
2020-01-14 19:52:09 +0000

    correct set_knownhosts

--- playbooks/set-knownhost.sh
+++ playbooks/set-knownhost.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
- 
- 
-# Declare an array of string with type
-declare -a StringArray=("web1" "web2" "lb"  )
- 
-# Iterate the string array using for loop
-for val in ${StringArray[@]}; do
-   echo addding $val to list of known hosts
-   ssh-keyscan $val >> ~/.ssh/known_hosts
-done
+
+declare -a StringArray=("web1" "web2" "lb" )
+
+for val in ${StringArray[@]}; do 
+   echo adding $val >> ~/.ssh/known_hosts
+done 
